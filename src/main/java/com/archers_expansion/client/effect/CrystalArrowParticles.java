c0287f7c0487e0ad4f597998f2bb8e44931a45fd
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-03-15 09:05:47 +0100

    Improve Particles

--- src/main/java/com/archers_expansion/client/effect/CrystalArrowParticles.java
+++ src/main/java/com/archers_expansion/client/effect/CrystalArrowParticles.java
@@ -1,24 +1,24 @@
-package com.archers_expansion.client;
+package com.archers_expansion.client.effect;
 
 import net.minecraft.entity.LivingEntity;
 import net.spell_engine.api.effect.CustomParticleStatusEffect;
 import net.spell_engine.api.spell.ParticleBatch;
 import net.spell_engine.particle.ParticleHelper;
 
-public class ChokingPoisonParticles implements CustomParticleStatusEffect.Spawner{
+public class CrystalArrowParticles implements CustomParticleStatusEffect.Spawner{
     private final ParticleBatch particles;
 
-    public ChokingPoisonParticles(int particleCount) {
+    public CrystalArrowParticles(int particleCount) {
         this.particles = new ParticleBatch(
-                "more_rpg_classes:gas_cloud",
-                ParticleBatch.Shape.SPHERE, ParticleBatch.Origin.LAUNCH_POINT,
-                null, particleCount, 0.05F, 0.1F, 360);
+                "spell_engine:snowflake",
+                ParticleBatch.Shape.SPHERE, ParticleBatch.Origin.CENTER,
+                null, particleCount, 0.1F, 0.3F, 0);
     }
 
     @Override
     public void spawnParticles(LivingEntity livingEntity, int amplifier) {
         var scaledParticles = new ParticleBatch(particles);
-        scaledParticles.count = (1);
+        scaledParticles.count *= (amplifier + 1);
         ParticleHelper.play(livingEntity.getWorld(), livingEntity, scaledParticles);
     }
 }
