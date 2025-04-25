033e53f96cbb43349bd192e6d3313fe80078ac74
ProfessorFichte <70310161+ProfessorFichte@users.noreply.github.com>
2025-04-25 11:58:30 +0200

    Add Datagen

--- src/main/java/com/archers_expansion/ArchersExpansionDataGenerator.java
+++ src/main/java/com/archers_expansion/ArchersExpansionDataGenerator.java
@@ -4,7 +4,13 @@ import com.archers_expansion.items.armors.Armors;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
+import net.spell_engine.api.item.armor.Armor;
 import net.spell_engine.rpg_series.datagen.RPGSeriesDataGen;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,6 +20,7 @@ public class ArchersExpansionDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 		pack.addProvider(ItemTagGenerator::new);
+		pack.addProvider(UnsmeltGenerator::new);
 	}
 
 	public static class ItemTagGenerator extends RPGSeriesDataGen.ItemTagGenerator {
@@ -24,6 +31,43 @@ public class ArchersExpansionDataGenerator implements DataGeneratorEntrypoint {
 		@Override
 		protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
 			generateArmorTags(Armors.entries);
+		}
+	}
+
+	public static class UnsmeltGenerator extends FabricRecipeProvider {
+		public UnsmeltGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+			super(output, registriesFuture);
+		}
+
+		public static int UNSMELT_TIME = 300;
+
+		@Override
+		public void generate(RecipeExporter exporter) {
+			disassembleArmor(exporter, Armors.deadeye_t1, Items.LEATHER);
+			disassembleArmor(exporter, Armors.war_archer_t1, Items.IRON_NUGGET);
+			disassembleArmor(exporter, Armors.tundra_hunter_t1, Items.LEATHER);
+			disassembleArmor(exporter, Armors.netherite_deadeye, Items.NETHERITE_SCRAP);
+			disassembleArmor(exporter, Armors.netherite_war_archer, Items.NETHERITE_SCRAP);
+			disassembleArmor(exporter, Armors.netherite_tundra_hunter, Items.NETHERITE_SCRAP);
+		}
+
+		private static void disassembleArmor(RecipeExporter exporter, Armor.Set armorSet, Item output) {
+			FabricRecipeProvider.offerSmelting(exporter,
+					armorSet.pieces(),
+					RecipeCategory.MISC,
+					output,
+					0.1f,
+					UNSMELT_TIME,
+					"disassemble"
+			);
+			FabricRecipeProvider.offerBlasting(exporter,
+					armorSet.pieces(),
+					RecipeCategory.MISC,
+					output,
+					0.1f,
+					UNSMELT_TIME / 2,
+					"disassemble"
+			);
 		}
 	}
 }
